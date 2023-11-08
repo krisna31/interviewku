c932382b682b122c204069b586fc5dd0f969680f
Krisna31 <krisnaaaputraaa@gmail.com>
2023-11-08 19:15:56 +0700

    BE ~ POSTMAN ~ update label and postman collection

--- backend/src/validator/authentication/schema.js
+++ backend/src/validator/authentication/schema.js
@@ -7,7 +7,7 @@ const joiPassword = Joi.extend(joiPasswordExtendCore);
 const PostAuthenticationPayloadSchema = Joi.object({
   email: Joi.string().email({ tlds: true }).required(),
   password: Joi.string().required(),
-});
+}).label('PostAuthenticationPayloadSchema');
 
 const PostAuthenticationResponseSchema = Joi.object({
   status: Joi.string().required(),
@@ -16,37 +16,38 @@ const PostAuthenticationResponseSchema = Joi.object({
     accessToken: Joi.string(),
     refreshToken: Joi.string(),
   }),
-});
+}).label('PostAuthenticationResponseSchema');
 
 const PutAuthenticationPayloadSchema = Joi.object({
   refreshToken: Joi.string().required(),
-});
+}).label('PutAuthenticationPayloadSchema');
+
 const PutAuthenticationResponseSchema = Joi.object({
   status: Joi.string().required(),
   message: Joi.string().required(),
   data: Joi.object({
     accessToken: Joi.string(),
   }),
-});
+}).label('PutAuthenticationResponseSchema');
 
 const DeleteAuthenticationPayloadSchema = Joi.object({
   refreshToken: Joi.string().required(),
-});
+}).label('DeleteAuthenticationPayloadSchema');
 
 const DeleteAuthenticationResponseSchema = Joi.object({
   status: Joi.string().required(),
   message: Joi.string().required(),
-});
+}).label('DeleteAuthenticationResponseSchema');
 
 const PutChangePasswordPayloadSchema = Joi.object({
   oldPassword: Joi.string().required(),
   newPassword: passwordValidation,
-});
+}).label('PutChangePasswordPayloadSchema');
 
 const PutChangePasswordResponseSchema = Joi.object({
   status: Joi.string().required(),
   message: Joi.string().required(),
-});
+}).label('PutChangePasswordResponseSchema');
 
 module.exports = {
   PostAuthenticationPayloadSchema,
