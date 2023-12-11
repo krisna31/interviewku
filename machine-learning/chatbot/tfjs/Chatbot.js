a1e666e7ec5f96102e67ae3c83eac554839eea43
Vanezkun <fanes23.pratama@mhs.mdp.ac.id>
2023-12-11 15:40:52 +0700

    ML ~ ADD ~ Tensorflow.js version of chatbot model

---
+++ machine-learning/chatbot/tfjs/Chatbot.js
@@ -0,0 +1,107 @@
+const tf = require('@tensorflow/tfjs-node');
+const fs = require('fs');
+const handler = tf.io.fileSystem('./model/model.json');
+const model = tf.loadLayersModel(handler);
+const tokenizerJson = fs.readFileSync('../tokenizer_dict_chatbot.json', 'utf8');
+const tokenizer = JSON.parse(tokenizerJson);
+const tagsClassJson = fs.readFileSync('../chatbot_result_decoder.json', 'utf8');
+const tagsClass = JSON.parse(tagsClassJson);
+
+
+const replace_words = [
+  ['online', ['daring', 'remote']],
+  ['meeting', ['gmeet', 'zoom']],
+  ['persiapan', ['persiapkan', 'disiapkan', 'dipersiapkan']],
+  ['cv', ['curiculum vitae', 'resume']],
+  ['penutup', ['closing statement']],
+  ['mereschedule', ['jadwal ulang', 'menjadwalkan ulang', 'mengubah jadwal', 'merubah jadwal', 'pindah jadwal']],
+  ['pewawancara', ['hrd']],
+  ['bagus', ['menarik', 'keren', 'tepat']],
+  ['kesalahan umum', ['kesalahan kecil']],
+  ['saran', ['tips']],
+  ['pakaian', ['baju', 'setelan', 'kostum']]
+];
+
+function preprocessingText(sentence) {
+  const filteredWords = sentence.toLowerCase().replace(/[^\w\d\s]/g, '');
+  const words = filteredWords.split(/\s+/);
+
+  const cleanedWords = [];
+  for (let word of words) {
+    let replaced = false;
+    for (let [replacement, target] of replace_words) {
+      if (target.includes(word)) {
+        cleanedWords.push(replacement);
+        replaced = true;
+      }
+    }
+    if (!replaced) {
+      cleanedWords.push(word);
+    }
+  }
+
+  return cleanedWords.join(' ');
+}
+
+function getKeyByValue(object, targetValue) {
+  for (let key in object) {
+    if (object.hasOwnProperty(key) && object[key] === targetValue) {
+      return key;
+    }
+  }
+  return null;
+}
+
+function tokenize(text, max_length) {
+  text = text.toLowerCase();
+  text = text.replace(/[!"#$%&()*+,-./:;<=>?@\[\\\]\^_`{|}~\t\n]/g, '')
+  let split_text = text.split(' ');
+  let tokens = [];
+  split_text = split_text.slice(0, max_length)
+
+  split_text.forEach(element => {
+    if (tokenizer[element] != undefined) {
+      tokens.push(tokenizer[element]);
+    }
+  });
+  let i = 0;
+  tokenized_text_segments = [];
+  while (i + 50 < Math.max(tokens.length, 100)) {
+    let new_slice = tokens.slice(i, i + 100);
+    while (new_slice.length < max_length) {
+      new_slice.push(0);
+    }
+    tokenized_text_segments.push(new_slice);
+    i = i + 50;
+  }
+  console.log(tokenized_text_segments);
+
+  return tokenized_text_segments;
+}
+
+async function predictText(answer) {
+  return new Promise((resolve, reject) => {
+    model.then(function (res) {
+      // preprocessing text
+      const processedText = preprocessingText(answer);
+      const tokenizedText = tokenize(processedText, 13);
+
+      // predict text
+      const result = res.predict(tf.tensor2d(tokenizedText));
+      const tagResult = result.argMax(-1).arraySync()[0];
+      // console.log(tagResult);
+
+      resolve(getKeyByValue(tagsClass, tagResult));
+    }).catch(function (err) {
+      reject(err);
+    });
+  });
+}
+
+const main = async () => {
+  const result = await predictText('apa yang harus disiapkan sebelum melakukan wawancara');
+  console.log(result);
+}
+
+main();
+
